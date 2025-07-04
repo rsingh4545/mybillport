41cada4d83f170c1dc59afdf41f9cd111c8d5d53
rsingh4545 <Randy.sandhu.rs@gmail.com>
2025-07-04 11:12:59 -0400

    Delete BillItem.js

--- BillItem.js
+++
@@ -1,9 +0,0 @@
-export default function BillItem({ name, amount, due }) {
-  return (
-    <div className="bill-item">
-      <p><strong>{name}</strong></p>
-      <p>Amount: ${amount}</p>
-      <p>Due: {due}</p>
-    </div>
-  );
-}
