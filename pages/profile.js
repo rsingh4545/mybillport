722d396be877762fd3a638cb2626db1594c49450
rsingh4545 <Randy.sandhu.rs@gmail.com>
2025-07-27 22:38:31 -0400

    profile.js

---
+++ pages/profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProfilePage = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Your Profile</h1>
+      <p className="text-gray-700">This is where your account settings and profile info will appear.</p>
+    </div>
+  );
+};
+
+export default ProfilePage;
